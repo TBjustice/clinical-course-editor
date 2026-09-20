1143e02d62fd4a6067fe77abf0b1f77b93739c8c
TrueBlueFeather <56857367+TBjustice@users.noreply.github.com>
2026-09-20 16:34:04 +0900

    refactor data tab code

--- src/components/main-section-tab/DataTab.tsx
+++ src/components/main-section-tab/DataTab.tsx
@@ -7,13 +7,134 @@ import { ProjectDataContext } from '../../contexts/ProjectContexts';
 import type { CliCTable } from '../../types/CliCTypes';
 import Dialog from '../ui/Dialog ';
 
+function TableConfig({ table, setTable }: {
+  table: CliCTable,
+  setTable: (newTable: CliCTable) => void
+}) {
+  const [renameDialogIndex, setRenameDialogIndex] = useState(-1);
+  const [addColumnDialogOpen, setAddColumnDialogOpen] = useState(false);
+  const [tempText, setTempText] = useState('');
+
+  function renameTable(newName: string) {
+    setTable({
+      ...table,
+      name: newName
+    });
+  }
+
+  function renameHeader(index: number, newName: string) {
+    setTable({
+      ...table,
+      header: table.header.map((item, i) => {
+        return i == index ? newName : item;
+      })
+    });
+  }
+
+  function addHeader(newName: string) {
+    setTable({
+      ...table,
+      header: [...table.header, newName],
+      data: table.data.map((item) => [...item, null])
+    });
+  }
+
+  return (
+    <section className={stylesCmn.editor_section}>
+      <header className={styles.table_name}>
+        <input
+          type="text" value={table.name}
+          onChange={(event) => {
+            renameTable(event.target.value)
+          }} />
+        <button className={styles.delete_button}>
+          <span className="material-icons-outlined">delete_forever</span>
+        </button>
+      </header>
+      <div className={styles.header_edit_wrap}>
+        {table.header.map((value, idx) => {
+          return (
+            <section key={idx}>
+              <header className={styles.header_edit_name}>
+                <div className={styles.header_name}>{value}</div>
+                <button
+                  className={`${styles.icon_button} has-tooltip`}
+                  onClick={() => {
+                    setTempText(value);
+                    setRenameDialogIndex(idx);
+                  }}>
+                  <span className="material-icons-outlined">edit</span>
+                  <div className="tooltip-left tooltip lang-en">Rename Column</div>
+                  <div className="tooltip-left tooltip lang-jp">行の名前を変更</div>
+                </button>
+                <button className={styles.delete_button}>
+                  <span className="material-icons-outlined">delete_forever</span>
+                </button>
+              </header>
+            </section>
+          );
+        })}
+        <button
+          className={styles.add_column_button}
+          onClick={() => {
+            setTempText('');
+            setAddColumnDialogOpen(true);
+          }}>
+          <span className='lang-en'>Add Column</span>
+          <span className='lang-jp'>行を追加</span>
+        </button>
+      </div>
+      <Dialog
+        isOpen={renameDialogIndex >= 0}
+        onCancelDialog={() => { setRenameDialogIndex(-1); }}>
+        <header>
+          <span className='lang-en'>Rename Column</span>
+          <span className='lang-jp'>行の名前を変更</span>
+        </header>
+        <input
+          type="text" name='rename-column-dialog' value={tempText}
+          onChange={(event) => { setTempText(event.target.value); }} />
+        <menu>
+          <button onClick={() => { setRenameDialogIndex(-1); }}>Cancel</button>
+          <button
+            onClick={() => {
+              renameHeader(renameDialogIndex, tempText);
+              setRenameDialogIndex(-1);
+            }}>
+            OK
+          </button>
+        </menu>
+      </Dialog>
+      <Dialog
+        isOpen={addColumnDialogOpen}
+        onCancelDialog={() => { setAddColumnDialogOpen(false); }}>
+        <header>
+          <span className='lang-en'>Add Column</span>
+          <span className='lang-jp'>行を追加</span>
+        </header>
+        <input
+          type="text" name='rename-column-dialog' value={tempText}
+          onChange={(event) => { setTempText(event.target.value); }} />
+        <menu>
+          <button onClick={() => { setAddColumnDialogOpen(false); }}>Cancel</button>
+          <button
+            onClick={() => {
+              addHeader(tempText);
+              setAddColumnDialogOpen(false);
+            }}>
+            OK
+          </button>
+        </menu>
+      </Dialog>
+    </section>
+  )
+}
+
 function DataTabEditor({ activeIndex, setActiveIndex }: { activeIndex: number, setActiveIndex: Dispatch<SetStateAction<number>> }) {
   const projectData = useContext(ProjectDataContext);
   if (!projectData) {
     throw new Error('ProjectData must be used within a provider');
   }
-  const [renameDialogIndex, setRenameDialogIndex] = useState(-1);
-  const [renamingText, setRenamingText] = useState('');
 
   const tables = projectData.value.map((item) => item.name);
   const activeTable = activeIndex < 0 ? undefined : projectData.value[activeIndex];
@@ -39,30 +160,6 @@ function DataTabEditor({ activeIndex, setActiveIndex }: { activeIndex: number, s
       }
       return item;
     }))
-  }
-
-  function addColumn(active: CliCTable) {
-    updateActiveTable({
-      ...active,
-      header: [...active.header, 'Untitled'],
-      data: active.data.map((item) => [...item, ''])
-    });
-  }
-
-  function renameTable(active: CliCTable, name: string) {
-    updateActiveTable({
-      ...active,
-      name: name
-    });
-  }
-
-  function renameHeader(active: CliCTable, index: number, name: string) {
-    updateActiveTable({
-      ...active,
-      header: active.header.map((item, idx) => (
-        idx === index ? name : item
-      ))
-    });
   }
 
   return (
@@ -92,69 +189,7 @@ function DataTabEditor({ activeIndex, setActiveIndex }: { activeIndex: number, s
         </button>
       </section>
       {activeTable && (
-        <section className={stylesCmn.editor_section}>
-          <header className={styles.table_name}>
-            <input
-              type="text" value={activeTable.name}
-              onChange={(event) => {
-                renameTable(activeTable, event.target.value)
-              }} />
-            <button className={styles.delete_button}>
-              <span className="material-icons-outlined">delete_forever</span>
-            </button>
-          </header>
-          <div className={styles.header_edit_wrap}>
-            {activeTable.header.map((value, idx) => {
-              return (
-                <section key={idx}>
-                  <header className={styles.header_edit_name}>
-                    <div className={styles.header_name}>{value}</div>
-                    <button
-                      className="has-tooltip"
-                      onClick={() => {
-                        setRenamingText(value);
-                        setRenameDialogIndex(idx);
-                      }}>
-                      <span className="material-icons-outlined">edit</span>
-                      <div className="tooltip-left tooltip lang-en">Rename Column</div>
-                      <div className="tooltip-left tooltip lang-jp">行の名前を変更</div>
-                    </button>
-                    <button className={styles.delete_button}>
-                      <span className="material-icons-outlined">delete_forever</span>
-                    </button>
-                  </header>
-                </section>
-              );
-            })}
-            <button
-              className={styles.add_column_button}
-              onClick={() => { addColumn(activeTable) }}>
-              <span className='lang-en'>Add Column</span>
-              <span className='lang-jp'>行を追加</span>
-            </button>
-          </div>
-          <Dialog
-            isOpen={renameDialogIndex >= 0}
-            onCancelDialog={() => { setRenameDialogIndex(-1); }}>
-            <header>
-              <span className='lang-en'>Rename Column</span>
-              <span className='lang-jp'>行の名前を変更</span>
-            </header>
-            <input
-              type="text" name='rename-column-dialog' value={renamingText}
-              onChange={(event) => { setRenamingText(event.target.value); }} />
-            <menu>
-              <button onClick={() => { setRenameDialogIndex(-1); }}>Cancel</button>
-              <button
-                onClick={() => {
-                  renameHeader(activeTable, renameDialogIndex, renamingText);
-                  setRenameDialogIndex(-1);
-                }}>
-                OK
-              </button>
-            </menu>
-          </Dialog>
-        </section>
+        <TableConfig table={activeTable} setTable={updateActiveTable} />
       )}
     </div>
   );
--- src/components/ui/Dialog .tsx
+++ src/components/ui/Dialog .tsx
@@ -4,17 +4,19 @@ import styles from "./Dialog.module.css"
 export default function Dialog({ isOpen, onCancelDialog, children }: { isOpen: boolean, onCancelDialog: CallableFunction, children: React.ReactNode }) {
   if (!isOpen) return undefined;
   return (
-    <div
-      className={styles.wrapper}
-      onClick={(event) => {
-        event.stopPropagation();
-        onCancelDialog();
-      }}>
+    <>
+      <div
+        className={styles.wrapper}
+        onClick={(event) => {
+          event.stopPropagation();
+          onCancelDialog();
+        }}>
+      </div>
       <div
         className={styles.content}
         onClick={(event) => { event.stopPropagation(); }}>
         {children}
       </div>
-    </div>
+    </>
   );
 }
